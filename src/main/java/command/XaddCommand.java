74bbd536b8db39dee0c5db53bd59c9d65965884f
Molik Agarwal <a.molik@iitg.ac.in>
2026-03-20 14:52:54 +0530

    Implemented IDs validation of streams

--- src/main/java/command/XaddCommand.java
+++ src/main/java/command/XaddCommand.java
@@ -12,6 +12,25 @@ public class XaddCommand implements Command {
     public void execute(List<String> input, OutputStream out, RedisStore store) throws Exception {
         String key = input.get(1);
         String id = input.get(2);
+
+        int curTime = Integer.parseInt(id.split("-")[0]);
+        int curSeq = Integer.parseInt(id.split("-")[1]);
+        
+        if(curTime == 0 && curSeq == 0){
+            out.write("-ERR The ID specified in XADD must be greater than 0-0\r\n".getBytes());
+            out.flush();
+            return;
+        }
+        else if(curTime < store.milisecondsTime || (curTime == store.milisecondsTime && curSeq <= store.sequenceNumber)){
+            out.write("-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n".getBytes());
+            out.flush();
+            return;
+        }
+        else{
+            store.milisecondsTime = curTime;
+            store.sequenceNumber = curSeq;
+        }
+
         ConcurrentHashMap<String , String> entries = new ConcurrentHashMap<>();
         entries.put("id", id);
         for(int i = 3 ; i < input.size() ; i+=2){
--- src/main/java/store/RedisStore.java
+++ src/main/java/store/RedisStore.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RedisStore {
+    public static Integer milisecondsTime = 0;
+    public static Integer sequenceNumber = 0;
     public static final ConcurrentHashMap<String , RedisData> data = new ConcurrentHashMap<>();
     public static final ConcurrentHashMap<String , ArrayDeque<String>> list = new ConcurrentHashMap<>();
     public static final ConcurrentHashMap<String , ConcurrentHashMap<String , String>> stream = new ConcurrentHashMap<>();
