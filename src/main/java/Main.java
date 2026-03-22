8afb60108e4310ea01646aab8f2cfec08077c851
Molik Agarwal <a.molik@iitg.ac.in>
2026-03-22 14:03:24 +0530

    Implemented Custom port Support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,23 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Starting Redis Clone on port 6379...");
-
+        
         // Instantiate your centralized architecture
         RedisStore store = new RedisStore();
         CommandRegistry registry = new CommandRegistry();
 
-        int port = 6379;
+        int port = 6379; // Default Redis port
+
+        //Custom port support
+        if(args.length > 0 && args[0].equals("--port")){
+            try {
+                port = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port number. Using default port 6379.");
+            }
+        }
+        System.out.println("Starting Redis Clone on port " + port + "...");
+        
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             serverSocket.setReuseAddress(true);
             
