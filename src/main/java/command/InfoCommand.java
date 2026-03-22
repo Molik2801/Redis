ab3d368ce1d110677456f3744fcc663b1122796a
Molik Agarwal <a.molik@iitg.ac.in>
2026-03-22 14:49:24 +0530

    Added replicaId and offset options

--- src/main/java/command/InfoCommand.java
+++ src/main/java/command/InfoCommand.java
@@ -11,9 +11,14 @@ public class InfoCommand implements Command{
     public void execute(List<String> input, OutputStream out, RedisStore store) throws Exception {
         StringBuilder info = new StringBuilder();
         if(input.get(1).equals("replication")){
-            String role = "role:master";
-            if(store.isSlave) role = "role:slave";
-            info.append("$" + role.length() + "\r\n" + role + "\r\n");
+            String role = "master";
+            if(store.isSlave) role = "slave";
+
+            String masterReplId = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+            String masterOffset = "0";
+
+            String finalInfo = "role:" + role + "\r\n" + "master_replid:" + masterReplId + "\r\n" + "master_repl_offset:" + masterOffset;
+            info.append("$" + finalInfo.length() + "\r\n" + finalInfo + "\r\n");
         }
         out.write(info.toString().getBytes());
         out.flush();
